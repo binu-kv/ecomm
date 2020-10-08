52918b9f36451b065994ba172167687aa3a6c6eb
Binu K.V <binuthilak@MPP.COM>
2020-10-08 14:09:57 +0530

    broken homepage to smaller components and finished styling

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import './App.css';
 
-import './homepage.component';
-import HomePage from './homepage.component';
+
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
