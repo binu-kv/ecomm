19842ee29387b932df99ac08507b118e7ec14b4f
Binu K.V <binuthilak@MPP.COM>
2020-10-19 16:37:19 +0530

    install redux,react-redux,redux-logger and reducers

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
 
+import store from './redux/store';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-     <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <provider store = {store}>
+    <React.StrictMode>
+      <BrowserRouter>
+         <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </provider>,
   document.getElementById('root')
 );
