cd28997d48673228e78127d966a36594e019551f
Binu K.V <binuthilak@MPP.COM>
2020-10-23 20:35:53 +0530

    added collections overview component,restructured shop componnent,added shop reducer in redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,13 @@
 import React from 'react';
 
-import SHOP_DATA from './shop.data';
+import CollectionsOverview from '../../component/collections-overview/collections-overview.component';
 
-import CollectionPreview from '../../component/collection-preview/collection-preview.component';
+const ShopPage = ({ collections }) => (
 
+<div className='shop-page'>
+        <CollectionsOverview />
+    </div>
+    );
 
-class ShopPage extends React.Component {
-    constructor(props){
-        super(props);
-
-        this.state ={
-            collections:SHOP_DATA
-        }
-    }
-
-render(){
-    const {collections} = this.state;
-    return(<div className='shop-page'>
-        {collections.map(({ id,...otherProps }) => (
-            <CollectionPreview key={id} {...otherProps} />
-        ))}
-    </div>);
-    }
-
-}
-
+ 
 export default ShopPage;
