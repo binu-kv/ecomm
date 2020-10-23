a117dbeebf9d5e8d5b205d99240f72ef116816a0
Binu K.V <binuthilak@MPP.COM>
2020-10-23 18:23:44 +0530

    implemented redux persistore in store.js,storage in root reducer,persistGate in App.js

--- src/component/cart-dropdown/cart-dropdown.component.jsx
+++ src/component/cart-dropdown/cart-dropdown.component.jsx
@@ -27,7 +27,7 @@ const CartDropdown = ({ cartItems,history,dispatch }) => (
         dispatch(toggleCartHidden());
       }}
     >
-      GO TO CHECKOUT
+      GO TO Cart
     </CustomButton>
   </div>
 );
