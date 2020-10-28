3b3a623f3b9401754ba9805a0d6e66811f90b167
Binu K.V <binuthilak@MPP.COM>
2020-10-28 11:16:48 +0530

    logo updated

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -9,7 +9,6 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 
-
 //import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { ReactComponent as Logo } from '../../assets/home-icon.svg';
 
